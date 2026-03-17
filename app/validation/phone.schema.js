e919592d5c809123d44f9024328645c9474113ba
yashsolanki12 <yash@ecodesoft.com>
2026-03-17 18:29:55 +0530

    Integrate country code drop and design chagne for settings page

--- app/validation/phone.schema.js
+++ app/validation/phone.schema.js
@@ -4,13 +4,14 @@ export const phoneSchema = z.object({
   phone_number: z
     .string()
     .min(1, { message: "Phone number is required" })
-    .regex(/^\d+$/, { message: "Only digits are allowed in phone number" }),
+    .refine((val) => val === "" || /^\d+$/.test(val), {
+      message: "Only digits are allowed in phone number",
+    }),
 
   country_code: z
     .string()
     .min(1, { message: "Country code is required." })
-    .regex(/^\+\d{1,4}$/, {
-      message:
-        "Country code must start with '+', 1 to 4 digits (e.g., +91).",
+    .refine((val) => val === "" || /^\+\d{1,5}/.test(val), {
+      message: "Country code must start with '+', 1 to 5 digits (e.g., +91).",
     }),
 });
